52ef823aeb9593c209c75e231038738abcb3587c
MikhailKovalenko <kovalenko.mikhail.vadim@gmail.com>
2025-08-27 14:19:42 +0300

    prettier: format twoSum

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,17 +1,16 @@
-function twoSum(nums,target) {
+function twoSum(nums, target) {
+  let targetSum;
 
-    let targetSum;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      targetSum = nums[i] + nums[j];
 
-        for(let i = 0; i < nums.length; i++){
-            for(let j = 0; j < nums.length; j++){
-              targetSum = (nums[i])+(nums[j]);
-              
-              if((target==targetSum)&&(i!==j)){
-                  return [i,j];           
-                } 
-            }
-        }
-};
+      if (target == targetSum && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+}
 // nums = [2,7,11,15];
 // target = 9;
 // console.log("Two Sum", twoSum(nums, target));
