f28ce877e9a0e1bb00c6f8f5d69c421c7517c084
MikhailKovalenko <kovalenko.mikhail.vadim@gmail.com>
2025-08-27 14:23:54 +0300

    prettier: format fizzBuzz

--- leetcode/fizzBuzz.js
+++ leetcode/fizzBuzz.js
@@ -1,23 +1,21 @@
-var fizzBuzz = function(n) {
-   
-    let array = [];
-    for (let i = 1; i <=n; i++) {
-        if ((i % 3 == 0)&&(i % 5 != 0)){
-            array.push("Fizz");
-                continue;
-        }if((i % 3 != 0)&&(i % 5 == 0)){
-            array.push("Buzz");
-            continue;
-        }if((i % 3 == 0)&&(i % 5 == 0)){
-            array.push("FizzBuzz");
-            continue;
-        }
-        stri = i.toString();
-        array.push(stri);
-       
-       
-    };
-     return array;
-
+var fizzBuzz = function (n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+      array.push("Fizz");
+      continue;
+    }
+    if (i % 3 != 0 && i % 5 == 0) {
+      array.push("Buzz");
+      continue;
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+      array.push("FizzBuzz");
+      continue;
+    }
+    stri = i.toString();
+    array.push(stri);
+  }
+  return array;
 };
 // console.log("fizzBuzz", fizzBuzz(15));
