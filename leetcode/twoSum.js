2922f2c6c476e1c755f304c5f85eb83969a2fac1
MikhailKovalenko <kovalenko.mikhail.vadim@gmail.com>
2025-09-23 18:36:34 +0300

    change type of variables

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,17 +1,14 @@
-function twoSum(nums,target) {
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      const targetSum = nums[i] + nums[j];
 
-    let targetSum;
-
-        for(let i = 0; i < nums.length; i++){
-            for(let j = 0; j < nums.length; j++){
-              targetSum = (nums[i])+(nums[j]);
-              
-              if((target==targetSum)&&(i!==j)){
-                  return [i,j];           
-                } 
-            }
-        }
-};
-// nums = [2,7,11,15];
-// target = 9;
-// console.log("Two Sum", twoSum(nums, target));
+      if (target == targetSum && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+}
+nums = [2, 7, 11, 15];
+target = 13;
+console.log("Two Sum", twoSum(nums, target));
